a672679bb7cc4b0b6b2d0b3914b715908bce6727
AryanRastogi7767 <aryanrastogi97@gmail.com>
2024-12-30 08:52:59 +0530

    Added hate speech detection project

---
+++ Major Project Files/py files/config.py
@@ -0,0 +1,9 @@
+# .gitignore should include reference to config.py
+
+keys = dict(
+    api_key = "27As4smYh6w60YXW0OhJLRP2o",
+    api_secret = "DMRjRFEHqAypR8zG5nmYI22BaXck7mz8G5SJQX1eCnsZ1G9Ptd", 
+    access_token = "1603820021700268033-oDuqHeJKWx1pQaTQPBjELrRmw17QyE", 
+    token_secret = "UmdMW4ZxQdmV2UzjD64I2KGVwGbsLWAq1el16bO6cRlqN",
+    bearer_token = "AAAAAAAAAAAAAAAAAAAAAOFSkgEAAAAA%2BpHtyoThDKWllmgLannaxH4pZ4M%3DxPo35ANt1m6LimpArlq4abm2UHGdJHXXrhuWKXYx0NGgC4AGjP"
+)
